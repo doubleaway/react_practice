42026784ae4e3504c9958d4f9c3867226fda34fe
doubleaway <67884860+doubleaway@users.noreply.github.com>
2021-02-21 01:09:52 +0900

    리액트 구구단

---
+++ game/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import ResPonsCheck from "./responsechecks/responseCheck"
+
+function App() {
+  return (
+   <ResPonsCheck/>
+  );
+}
+
+export default App;
